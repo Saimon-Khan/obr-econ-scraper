cffdca7548bda8446567787c430bf4d2073c75f8
Sai-96 <khansymon@gmail.com>
2025-08-11 00:41:26 +0100

    Extracted link of interest from url

--- scraper.py
+++ scraper.py
@@ -1,1 +1,46 @@
 # This script aims to scrape the detailed economic forecast data from the OBR website. 
+
+import requests
+from bs4 import BeautifulSoup
+import json
+import wget
+import os
+import pandas as pd
+import numpy as np
+
+
+# Set the url
+url = "https://obr.uk/efo"
+
+# The url rejects GET requests that do not specify a user agent. As a result, it returns 403 forbidden. 
+# Fetching user agent from developer tools from browser. F12 -> network -> click on first one listed -> look for user agent under headers. 
+headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:141.0) Gecko/20100101 Firefox/141.0"}
+
+# GET request with headers
+response = requests.get(url, headers = headers)
+
+# BeautifulSoup the response 
+soup = BeautifulSoup(response.text, "html.parser")
+
+# After some inspection, I am interested in the detailed forecasts for the economy
+# Only need to extract the urls associated with a download link. 
+links_to_docs = soup.find_all("a", class_ = "download-link")
+
+# Create a set to store unique urls
+link_of_interest = set()
+
+# Loop over link to find the href
+for link in links_to_docs:
+    # only interested in hrefs with the following string in them 
+    if "economic-and-fiscal-outlook-detailed-forecast-tables-economy" in link.get("href"):
+        # Save those to the set created earlier
+        link_of_interest.add(link.get("href"))
+
+
+# Path to save the data to
+path = os.getcwd() + "\\data"
+
+# Try to download / read the file - currently not working. Also tried wget but that didn't work either
+for link in link_of_interest:
+    pd.read_excel(link)
+
